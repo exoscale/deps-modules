361ac1d78b474183ca0dc335f32d40b5b7128ba0
Max Penet <max.penet@exoscale.ch>
2021-10-18 11:01:55 +0200

    no tests for now

--- test/exoscale/deps_modules_test.clj
+++
@@ -1,7 +0,0 @@
-(ns exoscale.deps-modules-test
-  (:require [clojure.test :refer :all]
-            [exoscale.deps-modules :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
