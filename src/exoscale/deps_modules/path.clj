28e320b1ecd8dcb745b7893bc6f97300a8c12cca
Pierre-Yves Ritschard <pyr@spootnik.org>
2022-05-25 10:33:49 +0200

    depreciate :modules-dir in favor of :modules-patterns
    The existing behavior is preserved, but :modules-patterns
    is introduced, defaulting to the same behavior than previously, allowing
    more elaborate globbing.

--- src/exoscale/deps_modules/path.clj
+++ src/exoscale/deps_modules/path.clj
@@ -1,10 +1,18 @@
 (ns exoscale.deps-modules.path
-  (:import (java.nio.file Path Paths Files FileVisitOption)))
+  (:require [clojure.java.io :as io])
+  (:import (java.io File)
+           (java.nio.file Path Paths Files FileVisitOption FileSystems)))
 
 (defn ^Path build
   [s & elements]
-  (if (instance? Path s)
+  (cond
+    (instance? Path s)
     s
+
+    (instance? File s)
+    (.toPath s)
+
+    :else
     (Paths/get (str s) (into-array String elements))))
 
 (defn ^Path parent
@@ -17,7 +25,7 @@
 
 (defn canonicalize
   "Resolve a file path relative to the file it was referenced in."
-  [module dst]
+  [dst module]
   (str
    (.relativize
     (.getParent (build module))
@@ -31,10 +39,32 @@
   [p]
   (-> (build p) .toFile .getName))
 
-(defn find-files
-  [path max-depth pred]
-  (->> (Files/walk (build path) (int max-depth) (into-array FileVisitOption []))
+(defn list-modules-files
+  [path project-file-name]
+  (for [module-dir (.listFiles (io/file path))
+        :let [path (build (str module-dir) project-file-name)
+              file (.toFile path)]
+        :when (and (.exists file) (.isFile file))]
+    file))
+
+(defn build-matcher
+  [root-path patterns]
+  (->> (for [p patterns]
+         (let [glob (str "glob:" p)
+               matcher (-> (FileSystems/getDefault)
+                           (.getPathMatcher glob))]
+           (fn [test-path]
+             (when (.isFile (.toFile test-path))
+               (let [path (.relativize root-path test-path)]
+                 (.matches matcher path))))))
+       (apply some-fn)))
+
+(defn glob-modules-files
+  [root-path patterns]
+  (->> (Files/walk (build root-path) 100 (into-array FileVisitOption []))
        (.iterator)
        (iterator-seq)
-       (filter pred)
-       (into [] (map #(.toFile ^Path %)))))
+       (filter (build-matcher (build root-path) patterns))
+       (map #(.toFile %))))
+
+
